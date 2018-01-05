cacd1372ad12cbf0242352b2957fc2a66c9df83b
mayqmh <may@nutonomy.com>
2018-01-05 18:04:02 +0800

    first commit

---
+++ src/AuthorizationService.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace Auth0Authorization
+{
+    public class AuthorizationService
+    {
+        private Dictionary<string, string[]> _scopePolicies;
+        public AuthorizationService(Dictionary<string,string[]> scopePolicies)
+        {
+            _scopePolicies = scopePolicies;
+        }
+        public void AddAuthorizationPolicies(IServiceCollection services, string authorityDomain, bool isAuthRequired = false)
+        {
+            services.AddAuthorization(options =>
+            {
+                var authDomainUrl = authorityDomain + ((authorityDomain[authorityDomain.Length-1]=='/') ? "" : "/");
+                if (isAuthRequired)
+                {   
+                    foreach(KeyValuePair<string,string[]> policyPermissions in _scopePolicies)
+                    {
+                        options.AddPolicy(policyPermissions.Key,
+                            policy => policy.Requirements.Add(new IntersectionPermissions(policyPermissions.Value,
+                            authDomainUrl))
+                        );
+                    }
+                }
+                else
+                {
+                    // Choose which policy to passthrough when authentication is not required.
+                    options.DefaultPolicy = new AuthorizationPolicyBuilder().RequireAssertion(_ => true).Build();
+                    foreach (KeyValuePair<string, string[]> policyPermissions in _scopePolicies)
+                    {
+                        options.AddPolicy(policyPermissions.Key, options.DefaultPolicy);
+                        
+                    }
+                }
+            });
+        }
+
+        private class IntersectionPermissions : AuthorizationHandler<IntersectionPermissions>, IAuthorizationRequirement
+        {
+            private readonly string _issuer;
+            private readonly IEnumerable<string> _policyScopes;
+            // field which contains permissions in jwt
+            private const string PermissionsField = "scope";
+
+            public IntersectionPermissions(IEnumerable<string> policyScopes, string issuer)
+            {
+                _policyScopes = policyScopes;
+                _issuer = issuer;
+            }
+
+            protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, IntersectionPermissions requirement)
+            {
+                if (!context.User.HasClaim(c => c.Type == PermissionsField && c.Issuer == _issuer))
+                {
+                    return Task.CompletedTask;
+                }
+
+                // Split the scopes string into an array
+                var userScopes = context.User.FindFirst(c => c.Type == PermissionsField && c.Issuer == _issuer).Value.Split(' ');
+
+                // Succeed if the user permissions is superset of this policy
+                if (!_policyScopes.Except(userScopes).Any())
+                {
+                    context.Succeed(requirement);
+                }
+
+                return Task.CompletedTask;
+            }
+        }
+    }
+}
---
+++ test/AuthorizationServiceTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Auth0Authorization;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Moq;
+
+namespace Auth0AuthorizationTest
+{
+    public class AuthorizationServiceTest
+    {
+        private readonly AuthorizationService _authorizationService;
+
+        public AuthorizationServiceTest()
+        {
+            var scopePolicies = new Dictionary<string,string[]>()
+            {
+                { "Read", new string[] { "read:messages" } },
+                { "Write", new string[] { "write:messages" } },
+                { "Admin", new string[] { "read:messages", "write:messages" } }
+            };
+            _authorizationService = new AuthorizationService(scopePolicies);
+        }
+
+        [Fact]
+        public async Task AllowedUser()
+        {
+            _authorizationService.AddAuthorizationPolicies(,"",false);
+            var user = new ClaimsPrincipal(
+                new ClaimsIdentity(
+                    new Claim[] { new Claim("Read", "read:messages") }
+                )
+            );
+            var allowed = await authService.AuthorizeAsync(user, "Basic");
+            Assert.True(allowed);
+        }
+    }
+}
